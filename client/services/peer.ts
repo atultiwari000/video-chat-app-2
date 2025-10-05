843761030a0a84dbaec0b21c59a58ad482a7b219
atultiwari000 <tiwari.000atul@gmail.com>
2025-10-05 21:35:00 +0545

    limiting stun/turn servers to 1 each

--- client/services/peer.ts
+++ client/services/peer.ts
@@ -15,24 +15,24 @@ class PeerService {
       iceServers: [
         // STUN servers
         { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:stun1.l.google.com:19302" },
+        // { urls: "stun:stun1.l.google.com:19302" },
         
         // Free TURN server (metered.ca - free tier)
-        {
-          urls: "turn:openrelay.metered.ca:80",
-          username: "openrelayproject",
-          credential: "openrelayproject",
-        },
+        // {
+        //   urls: "turn:openrelay.metered.ca:80",
+        //   username: "openrelayproject",
+        //   credential: "openrelayproject",
+        // },
         {
           urls: "turn:openrelay.metered.ca:443",
           username: "openrelayproject",
           credential: "openrelayproject",
         },
-        {
-          urls: "turn:openrelay.metered.ca:443?transport=tcp",
-          username: "openrelayproject",
-          credential: "openrelayproject",
-        },
+        // {
+        //   urls: "turn:openrelay.metered.ca:443?transport=tcp",
+        //   username: "openrelayproject",
+        //   credential: "openrelayproject",
+        // },
       ],
       iceCandidatePoolSize: 10,
     });
