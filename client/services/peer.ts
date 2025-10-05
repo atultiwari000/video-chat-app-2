75db6a6131a823ac76bf0bd32663a343a99a96ab
atultiwari000 <tiwari.000atul@gmail.com>
2025-10-05 22:44:37 +0545

    adding twilio server issue

--- client/services/peer.ts
+++ client/services/peer.ts
@@ -11,29 +11,11 @@ class PeerService {
   }
 
   private async createPeer() {
+    const response = await fetch("https://video-chat-app-2-ep0t.onrender.com/ice-servers");
+    const { iceServers } = await response.json();
+    
     this.peer = new RTCPeerConnection({
-      iceServers: [
-        // STUN servers
-        { urls: "stun:stun.l.google.com:19302" },
-        // { urls: "stun:stun1.l.google.com:19302" },
-        
-        // Free TURN server (metered.ca - free tier)
-        // {
-        //   urls: "turn:openrelay.metered.ca:80",
-        //   username: "openrelayproject",
-        //   credential: "openrelayproject",
-        // },
-        {
-          urls: "turn:openrelay.metered.ca:443",
-          username: "openrelayproject",
-          credential: "openrelayproject",
-        },
-        // {
-        //   urls: "turn:openrelay.metered.ca:443?transport=tcp",
-        //   username: "openrelayproject",
-        //   credential: "openrelayproject",
-        // },
-      ],
+      iceServers,
       iceCandidatePoolSize: 10,
     });
 
